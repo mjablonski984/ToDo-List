c8db609a5cb7063f91b875d76c93bf0b136cb48d
mjablonski984 <mrjab84@gmail.com>
2019-11-11 23:42:50 +0000

    Add procfile, update package.json and ports

--- app.js
+++ app.js
@@ -129,6 +129,12 @@ app.post('/about', (req, res) => {
   res.render('about');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+// To listen locally(port 3000) and on heroku set up port as:
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log('Server has started successfully');
 });
